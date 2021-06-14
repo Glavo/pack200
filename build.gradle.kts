f70eb2a7abe4d727fb110a5e7868d6c1efbfc308
Glavo <zjx001202@126.com>
2021-06-14 16:41:43 +0800

    init

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    `java-library`
+}
+
+group = "org.glavo"
+version = "0.1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+
+tasks.compileJava {
+    options.release.set(8)
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
